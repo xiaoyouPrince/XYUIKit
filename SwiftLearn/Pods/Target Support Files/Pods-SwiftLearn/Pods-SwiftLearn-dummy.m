37e4bbbdc84776ed24c55c94770afd3e1cfb22d4
xiaoyou.qu <xiaoyouPrince@163.com>
2021-09-25 00:41:10 +0800

    update .gitignore

--- SwiftLearn/Pods/Target Support Files/Pods-SwiftLearn/Pods-SwiftLearn-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftLearn : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftLearn
-@end
--- SwiftLearn/Pods/Target Support Files/XYInfomationSection/XYInfomationSection-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_XYInfomationSection : NSObject
-@end
-@implementation PodsDummy_XYInfomationSection
-@end
