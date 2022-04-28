01279193c8bebeb4a679414ba651a8bf3311a61b
xiaoyou.qu <xiaoyouPrince@163.com>
2022-04-28 21:00:42 +0800

    集成快速匹配业务

--- XYUIKit/XYUIKit/XYUIKit/Classes/Categories/String+XYAdd.swift
+++ XYUIKit/XYUIKit/XYUIKit/Classes/Categories/String+XYAdd.swift
@@ -30,4 +30,28 @@ public extension String {
         String.getAttributeString(with: self, font: font, textColor: textColor, lineSpace: lineSpace)
     }
     
+    /// 给当前字符串设置属性
+    /// - Parameter attrs: 要设置的属性集合
+    /// - Returns: 属性字符串
+    func addAttributes(attrs: [NSAttributedString.Key : Any]) -> NSAttributedString {
+        let tipAttriStr: NSMutableAttributedString = NSMutableAttributedString(string: self, attributes: attrs)
+        return tipAttriStr
+    }
+    
+    /// 通过正则给字符串匹配到的内容设置属性
+    /// - Parameters:
+    ///   - attrs: 要设置的属性集合
+    ///   - pattern: 正则，调用者保证其正确性 egg: 匹配所有竖线，为 "[|]"
+    /// - Returns: 返回一个属性字符串
+    func addAttributes(attrs: [NSAttributedString.Key : Any], withRegx pattern: String) -> NSAttributedString {
+        let result = NSMutableAttributedString(string: self)
+        let regx = try? NSRegularExpression(pattern: pattern, options: .caseInsensitive)
+        if let matches = regx?.matches(in: self, range: NSRange(location: 0, length: self.count)) {
+            for m in matches {
+                result.addAttributes(attrs, range: m.range)
+            }
+        }
+        return result
+    }
+    
 }
