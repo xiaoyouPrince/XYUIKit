f65b2a2e4850551ded92374f232ecf064c5e7096
xiaoyou.qu <xiaoyouPrince@163.com>
2022-05-06 14:23:46 +0800

    设置一些常量相关

--- XYUIKit/XYUIKit/XYUIKit/Classes/Categories/UIColor+XYAdd.swift
+++ XYUIKit/XYUIKit/XYUIKit/Classes/Categories/UIColor+XYAdd.swift
@@ -19,7 +19,26 @@ public func kHexColor(_ valueRGB: UInt, alpha: CGFloat) -> UIColor {
         alpha: alpha)
 }
 
+public func randomColor() -> UIColor {
+    return UIColor.randomColor()
+}
+
 public extension UIColor {
+    
+    static var random: UIColor {
+        return randomColor()
+    }
+    
+    // MARK:-便利构造方法
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat){
+        // MARK:-必须通过self调用显式的构造方法
+        self.init(red: r / 255.0, green: g/255.0, blue: b/255.0, alpha: 1.0)
+    }
+    
+    // MARK: - 返回一个随机色
+    class func randomColor() -> UIColor {
+        return UIColor.init(r: CGFloat(arc4random_uniform(255)), g: CGFloat(arc4random_uniform(255)), b: CGFloat(arc4random_uniform(255)))
+    }
     
     @objc static func xy_getColor(hex: Int) -> UIColor {
         let r = ((CGFloat)(hex >> 16 & 0xFF))
