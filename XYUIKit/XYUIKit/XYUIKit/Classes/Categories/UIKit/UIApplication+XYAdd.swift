b3818036cf67b90cb15c4795c5381d0f651d3611
xiaoyou.qu <xiaoyouPrince@163.com>
2024-02-03 01:50:51 +0800

    增加App挂起方法

--- XYUIKit/XYUIKit/XYUIKit/Classes/Categories/UIKit/UIApplication+XYAdd.swift
+++ XYUIKit/XYUIKit/XYUIKit/Classes/Categories/UIKit/UIApplication+XYAdd.swift
@@ -10,6 +10,27 @@ import UIKit
 public extension UIApplication {
     
     /// 获取 keyWindow
+    @available(iOS 13.0, *)
+    var keyWindow_: UIWindow? {
+        let connectedScenes = UIApplication.shared.connectedScenes
+            .filter{$0.activationState == .foregroundActive}
+            .compactMap { $0 as? UIWindowScene }
+        let window = connectedScenes.first?
+            .windows
+            .first{ $0.isKeyWindow }
+        return window
+    }
+    
+    /// 获取当前 App 的截图
+    var snapShot: UIImage? {
+        UIApplication.shared.getKeyWindow()?.snapshotImage
+    }
+    
+}
+
+public extension UIApplication {
+    
+    /// 获取 keyWindow
     /// - Returns: keyWindow
     func getKeyWindow() -> UIWindow? {
         if #available(iOS 15.0, *) {
@@ -22,23 +43,18 @@ public extension UIApplication {
         }
     }
     
-    /// 获取 keyWindow
-    @available(iOS 13.0, *)
-    var keyWindow_: UIWindow? {
-        let connectedScenes = UIApplication.shared.connectedScenes
-            .filter{$0.activationState == .foregroundActive}
-            .compactMap { $0 as? UIWindowScene }
-        let window = connectedScenes.first?
-            .windows
-            .first{ $0.isKeyWindow }
-        return window
+    
+    
+    /// 挂起 App
+    static func suspend() { shared.suspend() }
+    /// 挂起 App
+    func suspend() {
+        DispatchQueue.main.async {
+            if UIApplication.shared.applicationState != .background {
+                UIApplication.shared.perform(#selector(NSXPCConnection.suspend))
+            }
+        }
     }
-    
-    
-    /// 获取当前 App 的截图
-    var snapShot: UIImage? {
-        UIApplication.shared.getKeyWindow()?.snapshotImage
-    }
-    
+
 }
 
