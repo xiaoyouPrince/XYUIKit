8e742e9efd65b60b5e5f510396f3e3e9ee598c9a
xiaoyou.qu <xiaoyouPrince@163.com>
2024-02-04 17:04:28 +0800

    重构YYUIKit示例工程

---
+++ YYUIKitDemo/YYUIKitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YYUIKitDemo
+//
+//  Created by 渠晓友 on 2024/2/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
