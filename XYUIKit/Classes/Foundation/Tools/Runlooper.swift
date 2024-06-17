fb4a06366a245e73306b387c2a0f26c825060584
xiaoyou.qu <xiaoyouPrince@163.com>
2024-06-17 16:08:58 +0800

    Runlooper 新增一个指定loop 次数的 loop 方法

--- XYUIKit/Classes/Foundation/Tools/Runlooper.swift
+++ XYUIKit/Classes/Foundation/Tools/Runlooper.swift
@@ -14,6 +14,7 @@ import Foundation
 @objc public class Runlooper: NSObject {
     private override init() {}
     static private var keys = [String]()
+    static private var keyCountDict = [String: (Int, Int)]() // max current
     
     /// 开启一个事件循环, 按指定时间间隔执行回调
     /// - Note: 此函数调用之后不会停止, 需要谨慎使用
@@ -36,6 +37,37 @@ import Foundation
         DispatchQueue.global().asyncAfter(deadline: .now() + interval, execute: {
             if keys.contains(forKey) {
                 startLoop(forKey: forKey, interval: interval, callback: callback)
+            }
+        })
+    }
+    
+    /// 开启一个指定 Key 事件循环 (指定循环次数, 到次数自动停止)
+    /// - Note: 停止需要调用 ‘stopLoop(forKey: String)’ 函数
+    /// - NOte: 此函数区别于不指定循环次数的函数, 不要用同样的 key 来同时调用两个函数
+    /// - Parameters:
+    ///   - forKey: 指定循环的 Key
+    ///   - interval: 时间间隔(单位:S)
+    ///   - loopCount: 指定循环次数, 次数到了自动停止
+    ///   - callback: 你要执行的事件
+    @objc public static func startLoop(forKey: String, interval: TimeInterval, loopCount: Int, callback: @escaping (_ currentCount: Int)->()){
+        if !keys.contains(forKey) {
+            keys.append(forKey)
+            keyCountDict[forKey] = (loopCount, 0)
+        }
+        DispatchQueue.main.async {
+            let counts = keyCountDict[forKey]
+            let currentCount = counts?.1 ?? 0
+            keyCountDict[forKey] = (loopCount, currentCount + 1)
+            
+            if currentCount < loopCount {
+                callback(currentCount)
+            } else {
+                stopLoop(forKey: forKey)
+            }
+        }
+        DispatchQueue.global().asyncAfter(deadline: .now() + interval, execute: {
+            if keys.contains(forKey) {
+                startLoop(forKey: forKey, interval: interval, loopCount: loopCount, callback: callback)
             }
         })
     }
