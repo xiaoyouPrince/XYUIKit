08299c2575afcb20dfd2b6540968fc2b8da56511
渠晓友 <xiaoyouPrince@163.com>
2024-09-11 20:51:53 +0800

    优化底层获取当前控制器方法，主线程调用

--- XYUIKit/Classes/UIKit/Categories/UIViewController+XYAdd.swift
+++ XYUIKit/Classes/UIKit/Categories/UIViewController+XYAdd.swift
@@ -9,19 +9,39 @@ import Foundation
 
 
 /// 获取当前可见的控制器
-/// - Note: 需要注意的, 此方法适用于当前有 keyWindow 的场景, 如果没有找到 keyWindow 会导致 app crash, 在一些特殊场景慎用,可以先提前获取 currenVC 并暂存, 在需要的场景使用, 比如画中画场景系统会修改App.keywindow
+/// - Note: 需要注意的, 此方法适用于当前有 keyWindow 的场景, 如果没有找到 keyWindow 返回nil, 在一些特殊场景慎用,可以先提前获取 currenVC 并暂存, 在需要的场景使用, 比如画中画场景系统会修改App.keywindow
 /// - Returns: UIViewController
-public func currentVisibleController() -> UIViewController {
-    
-    var keyWindow: UIWindow? = nil
-    for window in UIApplication.shared.windows {
-        if window.isKeyWindow {
-            keyWindow = window
-            break
+public func currentVisibleController() -> UIViewController! {
+    if !Thread.isMainThread {
+        let semaphore = DispatchSemaphore(value: 0)
+        var keyWindow: UIWindow? = nil
+        var result: UIViewController? = nil
+        DispatchQueue.main.async {
+            for window in UIApplication.shared.windows {
+                if window.isKeyWindow {
+                    keyWindow = window
+                    break
+                }
+            }
+            result = findTopViewController(from: keyWindow)
+            semaphore.signal()
         }
+        semaphore.wait()
+        return result
+    } else {
+        var keyWindow: UIWindow? = nil
+        for window in UIApplication.shared.windows {
+            if window.isKeyWindow {
+                keyWindow = window
+                break
+            }
+        }
+        return findTopViewController(from: keyWindow)
     }
-    
-    var topController = keyWindow!.rootViewController!
+}
+
+fileprivate func findTopViewController(from keyWindow: UIWindow?) -> UIViewController? {
+    guard let keyWindow = keyWindow, var topController = keyWindow.rootViewController else { return nil }
     
     if let tabBarVC = topController as? UITabBarController {
         topController = tabBarVC.selectedViewController ?? topController
@@ -48,8 +68,8 @@ public func currentVisibleController() -> UIViewController {
 public extension UIViewController {
     
     /// 获取当前可见的控制器
-    /// - Note: 需要注意的, 此方法适用于当前有 keyWindow 的场景, 如果没有找到 keyWindow 会导致 app crash, 在一些特殊场景慎用,可以先提前获取 currenVC 并暂存, 在需要的场景使用, 比如画中画场景系统会修改App.keywindow
-    static var currentVisibleVC: UIViewController {
+    /// - Note: 需要注意的, 此方法适用于当前有 keyWindow 的场景, 如果没有找到 keyWindow 返回 nil, 在一些特殊场景慎用,可以先提前获取 currenVC 并暂存, 在需要的场景使用, 比如画中画场景系统会修改App.keywindow
+    static var currentVisibleVC: UIViewController! {
         currentVisibleController()
     }
     
