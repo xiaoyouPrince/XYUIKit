2d3e4c40e5c9be8521bce8780a963bffa13c3dd8
xiaoyou.qu <xiaoyouPrince@163.com>
2024-12-19 14:52:04 +0800

    Update Console.swift

--- Sources/Classes/Foundation/Tools/Console.swift
+++ Sources/Classes/Foundation/Tools/Console.swift
@@ -15,9 +15,9 @@ public struct Console {
     ///   - item: 要打印的对象,可以为一个,也可为一串
     ///   - filePath: 所在文件路径
     ///   - line: 所在行数
-    public static func log(_ item: Any..., filePath: String = #file, line: Int = #line) {
+    public static func log(_ item: Any..., filePath: String = #file, function: Selector = #function, line: Int = #line) {
         #if DEBUG
-        print("\(Date())[\(fileName(filePath))](\(line)):", item)
+        print("\(Date())[\(fileName(filePath)).\(function)](\(line)):", item)
         #endif
     }
     
@@ -26,9 +26,9 @@ public struct Console {
     ///   - item: 要打印的对象,可以为一个,也可为一串
     ///   - filePath: 所在文件路径
     ///   - line: 所在行数
-    public static func debugLog(_ item: Any..., filePath: String = #file, line: Int = #line) {
+    public static func debugLog(_ item: Any..., filePath: String = #file, function: Selector = #function, line: Int = #line) {
         #if DEBUG
-        debugPrint("\(Date())[\(fileName(filePath))](\(line)):", item)
+        debugPrint("\(Date())[\(fileName(filePath)).\(function)](\(line)):", item)
         #endif
     }
     
@@ -37,19 +37,19 @@ public struct Console {
     ///   - item: 要打印的对象,如果为 字典,数组,则会序列化输出
     ///   - filePath: 所在文件路径
     ///   - line: 所在行数
-    public static func log(_ item: @autoclosure() -> Any, filePath: String = #file, line: Int = #line) {
+    public static func log(_ item: @autoclosure() -> Any, filePath: String = #file, function: Selector = #function, line: Int = #line) {
         #if DEBUG
         let fileName = fileName(filePath)
         do {
             if JSONSerialization.isValidJSONObject(item()) {
                 let data = try JSONSerialization.data(withJSONObject: item(), options: .prettyPrinted)
                 let json = String(data: data, encoding: .utf8) ?? "json serialization print error"
-                print("\(Date())[\(fileName)](\(line)):", json)
+                print("\(Date())[\(fileName).\(function)](\(line)):", json)
             } else {
-                print("\(Date())[\(fileName)](\(line)):", item())
+                print("\(Date())[\(fileName).\(function)](\(line)):", item())
             }
         } catch {
-            print("\(Date())[\(fileName)](\(line)):", item())
+            print("\(Date())[\(fileName).\(function)](\(line)):", item())
         }
         #endif
     }
